f28c081c00f527e741b25e8986b3d029e2bc362e
alejandro-411 <castano.alejandro.3774@eam.edu.co>
2024-02-29 23:32:38 -0500

    agregadas clases User y ContactDetails

--- src/main/java/com/eam/demo/models/ContactDetails.java
+++ src/main/java/com/eam/demo/models/ContactDetails.java
@@ -1,5 +1,23 @@
 package com.eam.demo.models;
 
+import jakarta.persistence.*;
+
+@Table(name="ContactDetails")
+@Entity
 public class ContactDetails {
- private int contactDetailsId;
+	
+	@Id
+	@GeneratedValue()
+	private long contactDetailsId;
+
+	private String contactName;
+
+	private String contactNumber;
+
+	private String contactAddress;
+
+	private String contactEmail;
+
+	
+
 }
