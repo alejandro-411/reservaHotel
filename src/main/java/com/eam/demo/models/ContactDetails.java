4cadcc41469837bc26f48f8abc575f92432ce956
Angie-diaz-1 <diaz.angie.2352@eam.edu.co>
2024-03-02 12:54:38 -0500

    creacion contacDetails

--- src/main/java/com/eam/demo/models/ContactDetails.java
+++ src/main/java/com/eam/demo/models/ContactDetails.java
@@ -1,11 +1,13 @@
 package com.eam.demo.models;
 
+import java.util.List;
+
 import jakarta.persistence.*;
 
 @Table(name="ContactDetails")
 @Entity
 public class ContactDetails {
-	
+
 	@Id
 	@GeneratedValue()
 	private long contactDetailsId;
@@ -18,6 +20,75 @@ public class ContactDetails {
 
 	private String contactEmail;
 
+	// Relación OneToMany con la clase User
+	@OneToMany(mappedBy = "contactUser")
+	private List<User> users;
+	
 	
 
+	public ContactDetails() {
+		super();
+	}
+
+	public ContactDetails(long contactDetailsId, String contactName, String contactNumber, String contactAddress,
+			String contactEmail, List<User> users) {
+		super();
+		this.contactDetailsId = contactDetailsId;
+		this.contactName = contactName;
+		this.contactNumber = contactNumber;
+		this.contactAddress = contactAddress;
+		this.contactEmail = contactEmail;
+		this.users = users;
+	}
+
+	public long getContactDetailsId() {
+		return contactDetailsId;
+	}
+
+	public void setContactDetailsId(long contactDetailsId) {
+		this.contactDetailsId = contactDetailsId;
+	}
+
+	public String getContactName() {
+		return contactName;
+	}
+
+	public void setContactName(String contactName) {
+		this.contactName = contactName;
+	}
+
+	public String getContactNumber() {
+		return contactNumber;
+	}
+
+	public void setContactNumber(String contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+
+	public String getContactAddress() {
+		return contactAddress;
+	}
+
+	public void setContactAddress(String contactAddress) {
+		this.contactAddress = contactAddress;
+	}
+
+	public String getContactEmail() {
+		return contactEmail;
+	}
+
+	public void setContactEmail(String contactEmail) {
+		this.contactEmail = contactEmail;
+	}
+
+	public List<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
+
+
+	
 }
--- src/main/java/com/eam/demo/models/User.java
+++ src/main/java/com/eam/demo/models/User.java
@@ -1,5 +1,37 @@
 package com.eam.demo.models;
 
-public class User {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "User")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private String userId;
+	
+	  @Column(name = "username")
+	  private String userName;
+	  
+	  @Column(name = "userEmail")
+	   private String userEmail;
+	  
+	  @Column(name = "userPassword")
+	 private  String userPassword;
+	  
+	  @Column(name = "userPhoneNumber")
+	   private String userPhoneNumber;
+	  
+	   // Definición de la relación ManyToOne con la entidad ContactDetails
+	    @ManyToOne
+	    @JoinColumn(name = "ContactDetails_id", referencedColumnName = "contactDetailsId")
+	     private ContactDetails contactDetails;
+	  
+	  
 }
