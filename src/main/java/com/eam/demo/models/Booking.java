cd3472ee3e783ff594e2f2ca3a0df6393912afdd
Angie-diaz-1 <diaz.angie.2352@eam.edu.co>
2024-03-03 00:41:29 -0500

    booking and hotel

--- src/main/java/com/eam/demo/models/Booking.java
+++ src/main/java/com/eam/demo/models/Booking.java
@@ -27,19 +27,146 @@ public class Booking {
 	@Column(name = "totalReservationPrice")
 	double totalReservationPrice;
 
-
+	// Definición de la relación ManyToOne con la entidad user
+	@ManyToOne
+	@JoinColumn(name = "user_id", referencedColumnName = "userId")
 	User user;
+
+
+	// Definición de la relación ManyToOne con la entidad Hotel
+	@ManyToOne
+	@JoinColumn(name = "Hotel_id", referencedColumnName = "hotelId")
 	Hotel hotel;
+
+	// Relación OneToMany con la clase Room
+	@OneToMany(mappedBy = "booking")
+	private List<Room> rooms;
+
+
+
+	// Definición de la relación ManyToOne con la entidad ReserveStatus
+	@ManyToOne
+	@JoinColumn(name = "ReserveStatus_id", referencedColumnName = "idRerserveStatus")
+	ReserveStatus reserveStatus;
+
+
+
+	public Booking() {
+		super();
+	}
+
+
+
+	public Booking(int idBooking, Date reservationEntryDate, Date reservationDepartureDate,
+			double totalReservationPrice, User user, Hotel hotel, List<Room> rooms, ReserveStatus reserveStatus) {
+		super();
+		this.idBooking = idBooking;
+		this.reservationEntryDate = reservationEntryDate;
+		this.reservationDepartureDate = reservationDepartureDate;
+		this.totalReservationPrice = totalReservationPrice;
+		this.user = user;
+		this.hotel = hotel;
+		this.rooms = rooms;
+		this.reserveStatus = reserveStatus;
+	}
+
+
+
+	public int getIdBooking() {
+		return idBooking;
+	}
+
+
+
+	public void setIdBooking(int idBooking) {
+		this.idBooking = idBooking;
+	}
+
+
+
+	public Date getReservationEntryDate() {
+		return reservationEntryDate;
+	}
+
+
+
+	public void setReservationEntryDate(Date reservationEntryDate) {
+		this.reservationEntryDate = reservationEntryDate;
+	}
+
+
+
+	public Date getReservationDepartureDate() {
+		return reservationDepartureDate;
+	}
+
+
+
+	public void setReservationDepartureDate(Date reservationDepartureDate) {
+		this.reservationDepartureDate = reservationDepartureDate;
+	}
+
+
+
+	public double getTotalReservationPrice() {
+		return totalReservationPrice;
+	}
+
+
+
+	public void setTotalReservationPrice(double totalReservationPrice) {
+		this.totalReservationPrice = totalReservationPrice;
+	}
+
+
+
+	public User getUser() {
+		return user;
+	}
+
+
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+
+
+	public Hotel getHotel() {
+		return hotel;
+	}
+
+
+
+	public void setHotel(Hotel hotel) {
+		this.hotel = hotel;
+	}
+
+
+
+	public List<Room> getRooms() {
+		return rooms;
+	}
+
+
+
+	public void setRooms(List<Room> rooms) {
+		this.rooms = rooms;
+	}
+
+
+
+	public ReserveStatus getReserveStatus() {
+		return reserveStatus;
+	}
+
+
+
+	public void setReserveStatus(ReserveStatus reserveStatus) {
+		this.reserveStatus = reserveStatus;
+	}
+
 	
-	 // Relación OneToMany con la clase Room
-    @OneToMany(mappedBy = "booking")
-    private List<Room> rooms;
 	
 	
-	
-	 // Definición de la relación ManyToOne con la entidad ReserveStatus
-    @ManyToOne
-    @JoinColumn(name = "ReserveStatus_id", referencedColumnName = "idRerserveStatus")
-   ReserveStatus reserveStatus;
-    
 }
