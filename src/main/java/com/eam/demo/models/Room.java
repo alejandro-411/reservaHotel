147def066c838abb1e330e1c81838154a3a6230c
alejandro-411 <castano.alejandro.3774@eam.edu.co>
2024-03-02 19:28:45 -0500

    clase Room y RoomType agregadas y maepadas

---
+++ src/main/java/com/eam/demo/models/Room.java
@@ -0,0 +1,24 @@
+package com.eam.demo.models;
+
+import jakarta.persistence.*;
+
+@Table
+@Entity
+public class Room {
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	private long roomId;
+	
+	@Column(name = "roomNumber")
+	private int roomNumber;
+	
+	@Column(name = "pricePerNight")
+	private double pricePerNight;
+	
+	@Column(name = "availability")
+	private boolean avalability;
+	
+	@ManyToOne
+	@JoinColumn(name = "roomTypeId", referencedColumnName = "typerRoomId")
+	private RoomType roomType;
+}
